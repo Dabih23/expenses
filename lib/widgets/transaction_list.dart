90d401bc6bd25677ca0b14f0f074c81dd0019fa9
Roman Tkachenko <Roman-Turchenko2002@yandex.ru>
2022-08-14 13:42:50 +0300

    Update transaction_list.dart

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,63 +9,60 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: transactions.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No transactions added yet...'),
-                SizedBox(
-                  height: 20,
+    return transactions.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text('No transactions added yet...'),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                            child: Text(
-                                '\$${transactions[index].amount.toStringAsFixed(2)}')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                  //если () то функция анонимная (не принимает ничего)
-                  //в метод удаления передаем id нашей транзакции 
-                  //(элемента в списке) по индексу
-                      onPressed: () => deleteTx(transactions[index].id),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 5,
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6),
+                      child: FittedBox(
+                          child: Text(
+                              '\$${transactions[index].amount.toStringAsFixed(2)}')),
                     ),
                   ),
-                );
-              },
-              itemCount: transactions.length,
-              // children: transactions.map((t) {}).toList(),
-            ),
-    );
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    //если () то функция анонимная (не принимает ничего)
+                    //в метод удаления передаем id нашей транзакции
+                    //(элемента в списке) по индексу
+                    onPressed: () => deleteTx(transactions[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+            // children: transactions.map((t) {}).toList(),
+          );
   }
 }
