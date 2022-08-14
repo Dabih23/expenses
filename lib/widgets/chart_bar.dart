26426344db3d5e18ef72eb26f45b48ca7c08a549
Roman Tkachenko <Roman-Turchenko2002@yandex.ru>
2022-08-14 13:40:57 +0300

    Update chart_bar.dart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -9,48 +9,61 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-                 //перевод типа без знаков после ,
-          child: FittedBox(//умещает child в доступное пространство
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 1.0),
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20),
-                ),
+    //вводи ограничения размеров виджетов
+    return LayoutBuilder(
+      //constraints - доступные ограничения
+      builder: (ctx, constraints) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constraints.maxHeight * 0.15,
+              //перевод типа без знаков после ,
+              child: FittedBox(
+                //умещает child в доступное пространство
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
               ),
-              //создает поле которое равно доле размера другого поля
-              FractionallySizedBox(
-                heightFactor: spendingProc,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(20),
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              //максимальная высота
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1.0),
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                ),
+                  //создает поле которое равно доле размера другого поля
+                  FractionallySizedBox(
+                    heightFactor: spendingProc,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(label),
-      ],
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(label),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
